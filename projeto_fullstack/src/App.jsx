f3f5f809442cd8d510407c3f817663fe94a53c00
IGOR SILVA SCHIMITZ <igor_s_schimitz@estudante.sesisenai.org.br>
2025-09-29 21:43:21 -0300

    começo projeto fullstack

---
+++ projeto_fullstack/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import User from './components/User/user'
+import Menu from './components/Menu/Menu'
+
+function App() {
+  return (
+    <>
+      <User/>
+      <Menu/>
+    </>
+  )
+}
+
+export default App
